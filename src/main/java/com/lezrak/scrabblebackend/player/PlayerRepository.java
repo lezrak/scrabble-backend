0daaaf56261dccf0e27728ab3c5deed98f810d56
lezrak <olaf.durski@gmail.com>
2019-02-05 13:19:12 +0100

    added PlayerRepository class

---
+++ src/main/java/com/lezrak/scrabblebackend/player/PlayerRepository.java
@@ -0,0 +1,13 @@
+package com.lezrak.scrabblebackend.player;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PlayerRepository extends JpaRepository<Player, Long> {
+    List<Player> findAll();
+
+    Player findPlayerByNickname(String nickname);
+
+    Player findPlayerByEmail(String email);
+}
