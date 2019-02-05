ef5fe71303fda1501b980980012790e43e3b204c
lezrak <olaf.durski@gmail.com>
2019-02-05 14:11:05 +0100

    added PlayerDTO class

--- src/main/java/com/lezrak/scrabblebackend/ScrabbleBackendApplication.java
+++ src/main/java/com/lezrak/scrabblebackend/ScrabbleBackendApplication.java
@@ -1,5 +1,6 @@
 package com.lezrak.scrabblebackend;
 
+import com.lezrak.scrabblebackend.player.Player;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
---
+++ src/main/java/com/lezrak/scrabblebackend/player/PlayerDTO.java
@@ -0,0 +1,41 @@
+package com.lezrak.scrabblebackend.player;
+
+public class PlayerDTO {
+
+    private String email;
+    private String nickname;
+    private String password;
+
+    public PlayerDTO(String email, String nickname) {
+        this.email = email;
+        this.nickname = nickname;
+    }
+
+    public PlayerDTO(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
