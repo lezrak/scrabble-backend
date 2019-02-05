6090f8b0e618134bbdfa9f04a4c88c837aefed85
lezrak <olaf.durski@gmail.com>
2019-02-05 15:39:55 +0100

    added GameDTO class

---
+++ src/main/java/com/lezrak/scrabblebackend/game/GameDTO.java
@@ -0,0 +1,77 @@
+package com.lezrak.scrabblebackend.game;
+
+import com.lezrak.scrabblebackend.player.PlayerDTO;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+
+public class GameDTO {
+    private LinkedHashSet<PlayerStateDTO> players = new LinkedHashSet<>();
+
+    private HashMap<String, Character> boardState = new HashMap<>();
+
+    private int nextPlayer = -1;
+
+
+    private String name;
+
+    public GameDTO() {
+    }
+
+    public GameDTO(LinkedHashSet<PlayerStateDTO> players, HashMap<String, Character> boardState, int nextPlayer, String name) {
+        this.players = players;
+        this.boardState = boardState;
+        this.nextPlayer = nextPlayer;
+        this.name = name;
+    }
+
+    private class PlayerStateDTO {
+
+
+        private PlayerDTO player;
+
+        private int totalPoints = 0;
+
+        private int lastMovePoints = 0;
+
+        private ArrayList<Character> characters = new ArrayList<>();
+
+        PlayerStateDTO(PlayerDTO player) {
+            this.player = player;
+
+        }
+    }
+
+    public LinkedHashSet<PlayerStateDTO> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(LinkedHashSet<PlayerStateDTO> players) {
+        this.players = players;
+    }
+
+    public HashMap<String, Character> getBoardState() {
+        return boardState;
+    }
+
+    public void setBoardState(HashMap<String, Character> boardState) {
+        this.boardState = boardState;
+    }
+
+    public int getNextPlayer() {
+        return nextPlayer;
+    }
+
+    public void setNextPlayer(int nextPlayer) {
+        this.nextPlayer = nextPlayer;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- src/main/java/com/lezrak/scrabblebackend/player/PlayerMapper.java
+++ src/main/java/com/lezrak/scrabblebackend/player/PlayerMapper.java
@@ -14,4 +14,5 @@ public class PlayerMapper {
                 .map(PlayerMapper::toPlayerDTO)
                 .collect(Collectors.toList());
     }
+
 }
