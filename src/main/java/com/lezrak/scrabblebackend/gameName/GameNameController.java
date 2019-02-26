74cb60b08f17a52952d893d8cb8f9665ead87cc4
lezrak <olaf.durski@gmail.com>
2019-02-26 12:50:33 +0100

    updated GameName timeout

--- src/main/java/com/lezrak/scrabblebackend/gameName/GameNameController.java
+++ src/main/java/com/lezrak/scrabblebackend/gameName/GameNameController.java
@@ -1,6 +1,7 @@
 package com.lezrak.scrabblebackend.gameName;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class GameNameController {
 
     @CrossOrigin
     @PostMapping("/populate")
+    @Transactional(timeout = 1200)
     public void populate() {
         gameNameService.populate();
     }
