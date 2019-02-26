caa3ebbbd854ef3d50bf8fa8dca83ef969fed2f6
lezrak <olaf.durski@gmail.com>
2019-02-26 12:39:22 +0100

    optimized GameName loading

--- src/main/java/com/lezrak/scrabblebackend/gameName/GameNameServiceImpl.java
+++ src/main/java/com/lezrak/scrabblebackend/gameName/GameNameServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.stream.Collectors;
 
@@ -25,6 +26,7 @@ public class GameNameServiceImpl implements GameNameService {
     @Override
     public String generateName() {
         Random random = new Random();
+        // there is 2965277 words allowed in polish version of scrabble, all saved in gameNameRepository
         String name = gameNameRepository.findGameNameById(random.nextInt(2965277)).getName();
         while (gameRepository.findByName(name) != null) {
             name += random.nextInt(10);
@@ -40,7 +42,16 @@ public class GameNameServiceImpl implements GameNameService {
                 InputStream fStream = new ClassPathResource("slowa.txt").getInputStream();
                 DataInputStream in = new DataInputStream(fStream);
                 BufferedReader br = new BufferedReader(new InputStreamReader(in));
-                gameNameRepository.saveAll(br.lines().map(GameName::new).collect(Collectors.toList()));
+                String line;
+                ArrayList<String> list = new ArrayList<>();
+                while ((line = br.readLine()) != null) {
+                    list.add(line);
+                    //there is 2965277 words on the list, 329 is divider of 2965277, which allows us to use batching and save all words from list
+                    if (list.size() == 329) {
+                        gameNameRepository.saveAll(list.stream().map(GameName::new).collect(Collectors.toList()));
+                        list.clear();
+                    }
+                }
                 in.close();
             } catch (Exception e) {
                 System.err.println("Error: " + e.getMessage());
