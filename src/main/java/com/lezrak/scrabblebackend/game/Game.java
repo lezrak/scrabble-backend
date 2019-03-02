1695d87e4a88752116d4c947bc482ba20b1ae628
lezrak <olaf.durski@gmail.com>
2019-03-02 14:25:45 +0100

    updated Game - now using RestTemplate

--- src/main/java/com/lezrak/scrabblebackend/game/Game.java
+++ src/main/java/com/lezrak/scrabblebackend/game/Game.java
@@ -2,19 +2,15 @@ package com.lezrak.scrabblebackend.game;
 
 
 import com.lezrak.scrabblebackend.common.BaseEntity;
-import com.lezrak.scrabblebackend.exception.*;
+import com.lezrak.scrabblebackend.exception.GameAlreadyStartedException;
+import com.lezrak.scrabblebackend.exception.GameFullException;
+import com.lezrak.scrabblebackend.exception.NotYourTurnException;
+import com.lezrak.scrabblebackend.exception.PlayerNotInGameException;
 import com.lezrak.scrabblebackend.player.Player;
-import org.apache.http.HttpEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.utils.URIBuilder;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.persistence.*;
-import java.io.IOException;
-import java.net.URISyntaxException;
 import java.util.*;
 
 @Entity
@@ -50,7 +46,6 @@ public class Game extends BaseEntity {
         }
     }
 
-    //todo: substitute with REST Template
     public void makeMove(Long playerId, HashMap<String, Character> move) {
 
         players.sort(PlayerState::compareTo);
@@ -59,20 +54,15 @@ public class Game extends BaseEntity {
         }
 
 
-        URIBuilder builder = null;
-        int points = 0;
-        try {
-            builder = new URIBuilder("https://scrabble-ai-mock.herokuapp.com/move");
-            builder.setParameter("boardState", this.boardState.toString()).setParameter("move", move.toString());
-            CloseableHttpClient httpclient = HttpClients.createDefault();
-            HttpGet httpGet = new HttpGet(builder.build());
-            CloseableHttpResponse response = httpclient.execute(httpGet);
-            HttpEntity entity = response.getEntity();
-            points = Integer.valueOf(EntityUtils.toString(entity, "UTF-8"));
+        String transactionUrl = "https://scrabble-ai-mock.herokuapp.com/move";
 
-        } catch (IOException | URISyntaxException e) {
-            throw new ApplicationMaintenanceException();
-        }
+        UriComponentsBuilder builder = UriComponentsBuilder
+                .fromUriString(transactionUrl)
+                .queryParam("boardState", this.boardState.toString())
+                .queryParam("move", move.toString());
+
+        RestTemplate restTemplate = new RestTemplate();
+        Integer points = restTemplate.getForObject(builder.toUriString(), Integer.class);
 
 
         for (PlayerState p : players) {
@@ -82,7 +72,6 @@ public class Game extends BaseEntity {
         }
         nextPlayer = (nextPlayer + 1) % players.size();
     }
-
 
 
     public void addPlayer(Player player) {
